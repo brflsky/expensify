df709c5500b1db1cedef227be058200e695b7956
Rafalsky <rafbe@ymail.com>
2018-03-14 01:53:27 +0100

    total expenses functionality added

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
-
-
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpansifyDashboardPage = () => (
-  <div>From Dashboard
+  <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
